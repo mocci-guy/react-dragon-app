7fe8e75f3facb13865003bd2cfc2e81cb01b1297
hamburger <tik.shitting@gmail.com>
2022-09-29 21:05:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DragonDataSheet } from "./components/DragonDataSheet";
+import { DragonList } from "./components/DragonList";
+import './styles/App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
+
+  const [dragonData, setdragonData] = React.useState([]);
+  const [currentDragon, setCurrentDragon] = React.useState("none");
+
+  React.useEffect(() => {
+    fetch("https://api.spacexdata.com/v4/dragons")
+      .then(res => res.json())
+      .then(json => setdragonData(json))
+      .catch(error => console.log(error.message))
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem('dragonData', JSON.stringify(dragonData));
+  }, [dragonData]);
+
+  React.useEffect(() => {
+    const dragonData = JSON.parse(localStorage.getItem('dragonData'));
+    if (dragonData) {
+      setdragonData(dragonData);
+    }
+  }, []);
+
+  const openDragonDataSheet = (identifier) => {
+    const res = dragonData.indexOf(dragonData.find(item => item.id === identifier));
+    setCurrentDragon(res);
+  }
+
+  const clearCurrentDragonStatus = () => {
+    setCurrentDragon("none");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {
+        currentDragon === "none" ?
+          (<DragonList data={dragonData} openDragonDataSheet={openDragonDataSheet}/>)
+        :
+          (
+          <DragonDataSheet clearCurrentDragonStatus={clearCurrentDragonStatus} data={dragonData[currentDragon]} />
+          )
+          
+      }
+      
+
     </div>
   );
 }
